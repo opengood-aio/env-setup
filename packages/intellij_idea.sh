b34bb7452fd1ee4c406de353c45f8b5281ea343e
Chris Jaehnen <cjaehnen@me.com>
2020-12-30 15:32:01 -0500

    Update intellij package to fix plugin install issue

--- packages/intellij_idea.sh
+++ packages/intellij_idea.sh
@@ -17,25 +17,13 @@ function install_intellij_idea() {
 
     if [[ ! -d "${apps_dir}/IntelliJ IDEA.app" ]]; then
         write_info "Installing IntelliJ IDEA..."
-        brew cask list intellij-idea &>/dev/null || brew cask install intellij-idea
+        brew cask list intellij-idea &>/dev/null || brew install --cask intellij-idea
         write_success "Done!"
         write_blank_line
 
         get_intellij_idea_product_info
-        write_success "Done!"
-        write_blank_line
-
-        write_info "Installing Pivotal IDE preferences for IntelliJ IDEA...'"
-        cd_push "${workspace_dir}"/pivotal_ide_prefs/cli
-        ./bin/ide_prefs install --ide=intellij
-        cd_pop
-        write_success "Done!"
-        write_blank_line
-
-        install_intellij_idea_plugins
         install_intellij_idea_settings
-        write_success "Done!"
-        write_blank_line
+        install_intellij_idea_plugins
     else
         write_progress "IntelliJ IDEA is already installed"
         write_success "Done!"
@@ -49,27 +37,27 @@ function uninstall_intellij_idea() {
     get_intellij_idea_product_info
 
     write_info "Uninstalling IntelliJ IDEA..."
-    brew cask uninstall intellij-idea || { write_warning "WARNING! IntelliJ IDEA is not installed and cannot be uninstalled. Continuing on."; }
+    brew uninstall --cask intellij-idea || { write_warning "WARNING! IntelliJ IDEA is not installed and cannot be uninstalled. Continuing on."; }
     write_success "Done!"
     write_blank_line
 
-    write_info "Uninstalling IntelliJ IDEA cache..."
+    write_info "Deleting IntelliJ IDEA cache directory..."
     rm -Rf "${intellij_idea_cache_dir}"
     write_success "Done!"
     write_blank_line
 
-    write_info "Uninstalling IntelliJ IDEA logs..."
+    write_info "Deleting IntelliJ IDEA logs directory..."
     rm -Rf "${intellij_idea_logs_dir}"
     write_success "Done!"
     write_blank_line
 
-    write_info "Uninstalling IntelliJ IDEA plugins..."
+    write_info "Deleting IntelliJ IDEA plugins directory..."
     rm -Rf "${intellij_idea_plugins_dir}"
     write_success "Done!"
     write_blank_line
 
-    write_info "Uninstalling IntelliJ IDEA preferences..."
-    rm -Rf "${intellij_idea_prefs_dir}"
+    write_info "Deleting IntelliJ IDEA settings directory..."
+    rm -Rf "${intellij_idea_settings_dir}"
     write_success "Done!"
     write_blank_line
 }
@@ -85,10 +73,21 @@ function get_intellij_idea_product_info() {
     intellij_idea_build="IU-${intellij_idea_build_version}"
 
     intellij_idea_product_dir=IntelliJIdea${intellij_idea_version}
+    intellij_idea_app_support_dir="${app_support_dir}/JetBrains"
+    intellij_idea_settings_dir="${intellij_idea_app_support_dir}/${intellij_idea_product_dir}"
+    intellij_idea_plugins_dir="${intellij_idea_app_support_dir}/${intellij_idea_product_dir}/plugins"
     intellij_idea_cache_dir="${caches_dir}/JetBrains/${intellij_idea_product_dir}"
     intellij_idea_logs_dir="${logs_dir}/JetBrains/${intellij_idea_product_dir}"
-    intellij_idea_plugins_dir="${app_support_dir}/JetBrains/${intellij_idea_product_dir}/plugins"
-    intellij_idea_prefs_dir="${app_support_dir}/JetBrains/${intellij_idea_product_dir}"
+
+    write_info "IntelliJ IDEA version: ${intellij_idea_version}"
+    write_info "IntelliJ IDEA build version: ${intellij_idea_build_version}"
+    write_info "IntelliJ IDEA build: ${intellij_idea_build}"
+    write_info "IntelliJ IDEA product directory: ${intellij_idea_product_dir}"
+    write_info "IntelliJ IDEA app spupport directory: ${intellij_idea_app_support_dir}"
+    write_info "IntelliJ IDEA settings directory: ${intellij_idea_settings_dir}"
+    write_info "IntelliJ IDEA plugins directory: ${intellij_idea_plugins_dir}"
+    write_info "IntelliJ IDEA cache directory: ${intellij_idea_cache_dir}"
+    write_info "IntelliJ IDEA logs directory: ${intellij_idea_logs_dir}"
 
     write_success "Done!"
     write_blank_line
@@ -100,23 +99,19 @@ function download_intellij_idea_plugin() {
     local file="$3"
 
     local uri="${intellij_idea_plugins_api_base_uri}?action=download&id=${id}&build=${intellij_idea_build}"
+    write_info "IntelliJ IDEA plugin '${name}' download URI: '${uri}'"
 
-    write_progress "Downloading IntelliJ IDEA plugin '${name}' from '${uri}'..."
-    curl -o "${file}" "${uri}"
-    write_success "Done!"
-    write_blank_line
-}
-
-function install_intellij_idea_plugin() {
-    local name="$1"
-    local file="$2"
-
-    if [[ ! -d "${intellij_idea_plugins_dir}" ]]; then
-        mkdir -p "${intellij_idea_plugins_dir}"
+    write_progress "Downloading IntelliJ IDEA plugin '${name}' from '${uri}' with output filename '${file}'..."
+    curl -vL -o "${file}" "${uri}"
+    if [[ -f "${file}" ]]; then
+    	write_info "Completed download of IntelliJ IDEA plugin '${name}' to '${file}'"
+    else
+    	write_warning "Unable to download IntelliJ IDEA plugin '${name}' to '${file}'"
     fi
 
-    write_progress "Installing IntelliJ IDEA plugin '${name}' to '${intellij_idea_plugins_dir}'..."
-    cp "${file}" "${intellij_idea_plugins_dir}"
+    write_progress "Unzipping IntelliJ IDEA plugin '${name}' from output filename '${file}'..."
+    unzip -o "${file}"
+
     write_success "Done!"
     write_blank_line
 }
@@ -125,18 +120,20 @@ function install_intellij_idea_plugins() {
     write_info "Installing IntelliJ IDEA plugins..."
 
     local plugins=()
-    plugins+=("BashSupport Pro,pro.bashsupport")
+    plugins+=("BashSupportPro,pro.bashsupport")
     plugins+=("Go,org.jetbrains.plugins.go")
     plugins+=("Ignore,mobi.hsz.idea.gitignore")
-    plugins+=("Kotest,kotest-plugin-intellij<")
-    plugins+=("Kotlin,org.jetbrains.kotlin")
-    plugins+=("LinesSorter,org.sylfra.idea.plugins.linessorter")
-    plugins+=("Lombok,Lombook%20Plugin")
+    plugins+=("Kotest,kotest-plugin-intellij")
     plugins+=("MarkdownNavigator,com.vladsch.idea.multimarkdown")
     plugins+=("Python,Pythonid")
-    plugins+=("Toml,org.toml.lang")
 
-    cd_push "${downloads_dir}"
+
+    if [[ ! -d "${intellij_idea_plugins_dir}" ]]; then
+    	write_progress "Creating IntelliJ IDEA plugins directory '${intellij_idea_plugins_dir}'..."
+        mkdir -p "${intellij_idea_plugins_dir}"
+    fi
+
+    cd_push "${intellij_idea_plugins_dir}"
 
     local plugin
     for plugin in "${plugins[@]}"; do
@@ -145,11 +142,9 @@ function install_intellij_idea_plugins() {
         local id=${parts[1]}
 
         local file
-        file=$(to_lower_case "${name}.jar")
+        file=$(to_lower_case "${name}.zip")
 
         download_intellij_idea_plugin "${name}" "${id}" "${file}"
-        install_intellij_idea_plugin "${name}" "${file}"
-        rm -f "${file}"
     done
     unset plugin
 
@@ -157,17 +152,19 @@ function install_intellij_idea_plugins() {
 }
 
 function install_intellij_idea_settings() {
-    write_progress "Installing IntelliJ IDEA settings to '${intellij_idea_prefs_dir}'..."
+    write_progress "Installing IntelliJ IDEA settings to '${intellij_idea_settings_dir}'..."
 
-    rm -Rf "${intellij_idea_prefs_dir}"
-    if [[ ! -d "${intellij_idea_prefs_dir}" ]]; then
-        mkdir -p "${intellij_idea_prefs_dir}"
+    rm -Rf "${intellij_idea_settings_dir}"
+    if [[ ! -d "${intellij_idea_settings_dir}" ]]; then
+    	write_progress "Creating IntelliJ IDEA settings directory '${intellij_idea_settings_dir}'..."
+        mkdir -p "${intellij_idea_settings_dir}"
     fi
 
-    cp resources/intellij_idea_settings.zip "${prefs_dir}"
-    cd_push "${prefs_dir}"
+    cp resources/intellij_idea_settings.zip "${intellij_idea_app_support_dir}"
+    cd_push "${intellij_idea_app_support_dir}"
     unzip -o intellij_idea_settings.zip -d intellij_idea_settings
-    cp -R intellij_idea_settings/* "${intellij_idea_prefs_dir}"/
+    rm -Rf intellij_idea_settings/__MACOSX
+    cp -R intellij_idea_settings/* "${intellij_idea_settings_dir}"/
     rm -Rf intellij_idea_settings/
     rm -f intellij_idea_settings.zip
     cd_pop
