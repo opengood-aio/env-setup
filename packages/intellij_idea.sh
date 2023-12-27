2d9c5e5fc53575361855079f482c6a6cdc9cf238
Chris Jaehnen <cjaehnen@me.com>
2023-12-27 17:06:57 -0500

    Fix IntelliJ IDEA package

--- packages/intellij_idea.sh
+++ packages/intellij_idea.sh
@@ -1,6 +1,6 @@
 source "${setup_dir}"/modules/jetbrains.sh
 
-get_pycharm_dependencies() {
+get_intellij_idea_dependencies() {
     write_info "Getting JetBrains IntelliJ IDEA package dependencies to install..."
 
     local dependencies=()
@@ -12,10 +12,10 @@ get_pycharm_dependencies() {
     echo "${array#*=}"
 }
 
-install_pycharm() {
+install_intellij_idea() {
     install_jetbrains_product "${jetbrains_intellij_idea_ultimate_product_code}" jetbrains_intellij_idea_plugins
 }
 
-uninstall_pycharm() {
+uninstall_intellij_idea() {
     uninstall_jetbrains_product "${jetbrains_intellij_idea_ultimate_product_code}"
 }
