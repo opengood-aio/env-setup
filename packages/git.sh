3c934c26fe05f1e13178341799415952efb9b1e5
Chris Jaehnen <cjaehnen@users.noreply.github.com>
2025-04-18 16:08:54 -0400

    Update Git aliases

--- packages/git.sh
+++ packages/git.sh
@@ -107,7 +107,7 @@ install_git() {
         git config --global alias.lol "log --graph --decorate --oneline"
         git config --global alias.lola "log --graph --decorate --oneline --all"
         git config --global alias.pl "pull -r"
-        git config --global alias.ps "!git status && git add -A && git status && git commit -S -m \"$1\" && git pull -r && git push -u origin && git status"
+        git config --global alias.ps "!git status && git add -A && git status && git commit -S -m \"$1\" && git pull -r && git push -u origin \"$(git symbolic-ref --short HEAD)\" && git status"
         git config --global alias.ra "rebase --abort"
         git config --global alias.rc "rebase --continue"
         git config --global alias.rum "rebase main@{u}"
